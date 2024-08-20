0c7dd9f8004f3e1089f51248d15a4c9ff43b710b
Bhushan Udupa <bhushanudupa@gmail.com>
2024-08-20 13:21:23 +0530

    Completed Assignment 1 in Lex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    AssignmentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [HelloComponent]
+  bootstrap: [AssignmentComponent]
 })
 export class AppModule { }
